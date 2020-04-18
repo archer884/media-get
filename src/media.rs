cb877802304b82a6d12f0f3fb9f7a46905a8d09c
J/A <archer884@gmail.com>
2020-04-17 23:33:33 -0500

    Maybe this is cleaner

--- src/media.rs
+++ src/media.rs
@@ -3,12 +3,20 @@ use std::collections::VecDeque;
 use std::io::{self, Read};
 use url::Url;
 
+static USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+
 /// A media provider gives access to media from a certain source or source type.
 ///
 /// In addition to providing validation for urls, the provider serves as a factory for any
 /// associated media accessors.
 trait Provider {
     fn is_valid(&self, url: &Url) -> bool;
+    
+    fn configure_client(&self, builder: reqwest::blocking::ClientBuilder) -> Client {
+        builder.user_agent(USER_AGENT).build().unwrap()
+    }
+
     fn get_accessor(url: &Url) -> Box<dyn Accessor>;
 }
 
@@ -17,21 +25,20 @@ trait Provider {
 /// Generally, an accessor will fetch a page of media urls and this page will be processed before
 /// another page is requested. This is done to improve compatibility with rate limits, and to
 /// enable consumers to control when urls are consumed.
-///
-/// An accessor should also provide a properly-configured client for use by the consumer.
 trait Accessor {
-    fn client(&self) -> &Client;
-    fn get_page(&mut self) -> Option<Result<Vec<String>>>;
+    fn get_page(&mut self, client: &Client) -> Result<VecDeque<String>>;
 }
 
 struct TaskProvider<T> {
+    client: Client,
     accessor: T,
     current: VecDeque<String>,
 }
 
 impl<T: Accessor> TaskProvider<T> {
-    fn new(accessor: T) -> Self {
+    pub fn new(accessor: T, client: Client) -> Self {
         TaskProvider {
+            client,
             accessor,
             current: VecDeque::new(),
         }
@@ -61,7 +68,7 @@ impl<T: Accessor> Iterator for TaskProvider<T> {
         use reqwest::header::CONTENT_DISPOSITION;
 
         match self.current.pop_front() {
-            Some(url) => match self.accessor.client().get(&url).send() {
+            Some(url) => match self.client.get(&url).send() {
                 Ok(response) => {
                     let disposition = response.headers().get(CONTENT_DISPOSITION).and_then(|h| {
                         let disposition = h.to_str().ok()?;
@@ -81,10 +88,10 @@ impl<T: Accessor> Iterator for TaskProvider<T> {
                 Err(e) => return Some(Err(e.into())),
             },
 
-            None => match self.accessor.get_page()? {
+            None => match self.accessor.get_page(&self.client) {
                 Err(e) => return Some(Err(e)),
                 Ok(page) => {
-                    self.current = page.into();
+                    self.current = page;
                     self.next()
                 }
             },
